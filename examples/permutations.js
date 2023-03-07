c60d28b8caaa092ef6835fbd465ffdc4e48b8b37
felixdrp <felixdrp@gmail.com>
2023-03-07 10:26:42 +0000

    refactor: use index instead of field
    Resolve possible conflicts if there exists multiple fields with the same string.

--- examples/permutations.js
+++ examples/permutations.js
@@ -5,35 +5,39 @@
  * Recursive function
  */
 
-a = [ "Insulin", "Test", "Diagnosis" ]
 /**
  * @function getPermutations
  * @param {array} data - Array of information
  * @returns {array} permutations from data
+ * 
  * @description Get array permutations
  */
+
 getPermutations = (data) => {
   if (data.length == 1) return data[0]
   let total = []
-  for (let field of data) {
-		// get mutations for the subarray wihtout field
-    let permutations = getPermutations(data.filter(item => item !== field))
+  for (let index in data) {
+		// get mutations for the subarray wihtout field (data[index])
+    let permutations = getPermutations(data.filter((item, idx) => idx != index))
 		// then add field to all possible mutations    
     if (Array.isArray(permutations)) {
-			// Add field to each possible permutation
-    	permutations.forEach(item => total.push([field, ...item]))      
+			// if it is an array then add field (data[index]) to each possible permutation
+    	permutations.forEach(item => total.push([data[index], ...item]))      
     } else {
-			// If it is a value add it directly
-      total.push([field, permutations])
+			// If it is a value add it directly. Reached a leaf of tree (The end of the tree)
+      total.push([data[index], permutations])
     }
   }
   return total
 }
 
+/**
+ * Example:
+
+a = [ "Insulin", "Test", "Diagnosis" ]
+
 getPermutations(a)
 
-/**
- * It will return:
 Array(6) [ (3) […], (3) […], (3) […], (3) […], (3) […], (3) […] ]
 0: Array(3) [ "Insulin", "Test", "Diagnosis" ]
 ​1: Array(3) [ "Insulin", "Diagnosis", "Test" ]
@@ -41,4 +45,5 @@ Array(6) [ (3) […], (3) […], (3) […], (3) […], (3) […], (3) […] ]
 ​3: Array(3) [ "Test", "Diagnosis", "Insulin" ]
 ​4: Array(3) [ "Diagnosis", "Insulin", "Test" ]
 ​5: Array(3) [ "Diagnosis", "Test", "Insulin" ]
- */
+
+*/
